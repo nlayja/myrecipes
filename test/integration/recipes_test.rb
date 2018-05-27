25471975d0f4d87070df96770b1e876be602ee85
nlayja <nlayja@gmail.com>
2018-05-27 18:36:55 -0500

    add assert_response in recipes_test.rb

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes index" do
     get recipes_path
-    #assert_response :success
+    assert_response :success
   end
 end
