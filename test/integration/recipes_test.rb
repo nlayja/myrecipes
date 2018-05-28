95c636108f003333ab9f2c7d16d9df8fb3d2df91
nlayja <nlayja@gmail.com>
2018-05-28 13:41:30 -0500

    complete show recipe

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -7,6 +7,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     @recipe2 = @chef.recipes.build(name: "fish and potatoes or tomatoes", description: "delicious fish with some rice and potatoes or tomatoes, ready for 15 minutes")
     @recipe2.save
   end
+
   test "should get recipes index" do
     get recipes_path
     assert_response :success
@@ -15,7 +16,15 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_path
     assert_template 'recipes/index'
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+  
+  test "should get recipe show" do
+    get recipes_path(@recipe)
+    assert_template 'recipes/show'
     assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, respose.body
   end
 end
