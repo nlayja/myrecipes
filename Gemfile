b415fc3d61425e02787269a6bd87eaacf6787162
nlayja <nlayja@gmail.com>
2018-05-28 10:59:27 -0500

    add info in table recipe

--- Gemfile
+++ Gemfile
@@ -8,7 +8,6 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
-gem 'devise', '~> 4.1'
 gem 'jquery-rails'
 gem 'bootstrap-sass', '~> 3.3.7'
 # Use sqlite3 as the database for Active Record
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class RecipesTest < ActionDispatch::IntegrationTest
   def setup
-    @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com")
-    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
-    @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
+    @chef = Chef.create!(chefname: "pedro", email: "pedro@example.com")
+    @recipe = Recipe.create(name: "fish and vegetable green", description: "great vegetable green, oil for 10 minutes", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "fish and potatoes or tomatoes", description: "delicious fish with some rice and potatoes or tomatoes, ready for 15 minutes")
     @recipe2.save
   end
   test "should get recipes index" do
@@ -15,15 +15,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_path
     assert_template 'recipes/index'
-    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
-    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
-  end
-  
-  test "should get recipes show" do
-    get recipe_path(@recipe)
-    assert_template 'recipes/show'
     assert_match @recipe.name, response.body
-    assert_match @recipe.description, response.body
-    assert_match @chef.chefname, response.body
+    assert_match @recipe2.name, response.body
   end
 end
