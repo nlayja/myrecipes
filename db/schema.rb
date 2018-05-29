4a89036d46fcee05eb3230e3d69bc234c74d642c
nlayja <nlayja@gmail.com>
2018-05-29 15:26:17 -0500

    Add ingredient model and many to many assoc with recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180529145111) do
+ActiveRecord::Schema.define(version: 20180529194022) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20180529145111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
